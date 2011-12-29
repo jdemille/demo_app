b98e38c47eeab7db71e69366985b7f28feb22966
jdemille <jdemille31@gmail.com>
2011-12-29 13:43:32 -0600

    added micropost resoucre

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-  #  respond_to do |format|
-   #   format.html # show.html.erb
-   #   format.json { render json: @user }
-   # end
+  respond_to do |format|
+   format.html # show.html.erb
+   format.json { render json: @user }
+   end
   end
 
   # GET /users/new
